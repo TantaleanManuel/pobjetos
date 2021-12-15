7ca00961585835ffa29802907e48dc9a009dddd8
TantaleanWongManuel <santiagowt@hotmail.com>
2021-12-15 13:07:46 -0500

    proyecto objetos ver 2

--- oop.js
+++ oop.js
@@ -1,13 +1,18 @@
-// nombre de la clase debe ser mayusculas
+// nombre de la clase mayusculas
+ // instancia de Book
 class Book{
-    constructor (title,autor,precio){
-    title="title";
-    autor="autor";
-   precio="precio";
-    }
- }
+    constructor(a,b,c){
+    this._title="1984";
+    this.autor="George Orwell";
+    this.precio=350;
 
- 
+
+
  // instancia de Book
  const book1= new Book1("1984", "G.O", 359);
  const book2= new Book1("Frankenstein", "M.S", 200);
+ 
+ console.log(book1._title);
+ console.log(book1._autor);
+ console.log(book1._precio);
+
