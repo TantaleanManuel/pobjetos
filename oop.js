e9995a825dac73697f9bc00c182711fde5ae2056
TantaleanWongManuel <santiagowt@hotmail.com>
2021-12-15 10:54:30 -0500

    proyecto objetos ver 1

---
+++ oop.js
@@ -0,0 +1,12 @@
+// nombre de la clase debe ser mayusculas
+class Book{
+    constructor (title,autor,precio){
+    title="title";
+    autor="autor";
+   precio="precio";
+    }
+ }
+
+ // instancia de Book
+ const book1= new Book1("1984", "G.O", 359);
+ const book2= new Book1("Frankenstein", "M.S", 200);
